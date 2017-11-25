56ea180675fc9f0a6038004f75a14af0ee687b7e
Pablo Alvarez Doval <pablo.doval@gmail.com>
2017-11-25 19:42:10 +0000

    Updated for CNTK 2.3

--- scripts/environment.bat
+++ scripts/environment.bat
@@ -4,5 +4,5 @@ if %errorlevel% neq 0 (
     echo "[ERROR]: Something wrong happened during the creation of the environment." 
     exit /b %ERRORLEVEL%
     ) else (
-        activate cntk22
+        activate cntk23-bigdatconflithuania
     )
